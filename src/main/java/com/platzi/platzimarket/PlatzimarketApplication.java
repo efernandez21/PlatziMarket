07a5c7a45a6eddf16a0d9890eda380bf677f8adc
elkinfernandez21 <elkinfernandez21@gmail.com>
2022-03-01 20:39:18 -0500

    Inicializacion proyecto

---
+++ src/main/java/com/platzi/platzimarket/PlatzimarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.platzimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatzimarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatzimarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/platzi/platzimarket/PlatzimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.platzimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatzimarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
